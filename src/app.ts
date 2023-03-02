20a35044364fed2708d7174f5d8b37365978af3f
Lee Sharon <lees@moveo.co.il>
2023-03-02 14:33:02 +0200

    division of app to functions

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,42 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-import express from 'express'
+import express, { Express } from 'express'
 import { json } from 'body-parser'
 import cookieParser from 'cookie-parser'
 import apiRoutes from './api/api.routes'
 import { NotFoundError } from './errors/not-found-error'
 import { errorHandler } from './middlewares/error-handler'
+import connect from './services/db.service'
+import { isEnvVarsExist } from './utils/generalUtils'
+import { morgan } from './logger'
 
 const app = express()
 
-app.use(json())
+const setUpMiddleware = (app: Express) => {
+    if (process.env.NODE_ENV !== 'test') {
+        connect()
+        if (!isEnvVarsExist()) throw new Error('need to setup env variables')
+        app.use(morgan.successHandler)
+        app.use(morgan.errorHandler)
+    }
+    app.use(json())
+    app.use(cookieParser())
+}
 
-app.use(cookieParser())
+const setUpRoutes = (app: Express) => {
+    app.use('/api', apiRoutes)
+}
 
-app.use('/api', apiRoutes)
+const setUpErrorHandling = (app: Express) => {
+    app.all('*', async () => {
+        throw new NotFoundError()
+    })
+    app.use(errorHandler)
+}
 
-app.all('*', async (req, res) => {
-    throw new NotFoundError()
-})
-
-app.use(errorHandler)
+setUpMiddleware(app)
+setUpRoutes(app)
+setUpErrorHandling(app)
 
 export default app
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,4 @@
 import app from './app'
-import connect from './services/db.service'
-import { isEnvVarsExist } from './utils/generalUtils'
-import { morgan } from './logger'
-
-if (process.env.NODE_ENV !== 'test') {
-    connect()
-    if (!isEnvVarsExist()) throw new Error('need to setup env variables')
-    app.use(morgan.successHandler)
-    app.use(morgan.errorHandler)
-}
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
